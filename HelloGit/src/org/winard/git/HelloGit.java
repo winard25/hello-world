7612527b4d71373a2acc98668a82587a72af6539
Winard <Winard@Internet>
2017-08-03 15:48:29 +0800

    initial commit HelloGit

---
+++ HelloGit/src/org/winard/git/HelloGit.java
@@ -0,0 +1,14 @@
+package org.winard.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		greetings();
+
+	}
+	
+	public static void greetings() {
+		System.out.println("Hello Git");
+	}
+
+}
