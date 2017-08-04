9b45fb6da3e2a579a55c14af7b935ced58036f9b
Winard <Winard@Internet>
2017-08-04 15:02:59 +0800

    test num

--- HelloGit/src/org/winard/git/DisplayNumbers.java
+++ HelloGit/src/org/winard/git/DisplayNumbers.java
@@ -3,7 +3,7 @@ package org.winard.git;
 public class DisplayNumbers {
 	
 	public void generateNumbers() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 15; i++) {
 			System.out.println(i);
 		}
 	}
