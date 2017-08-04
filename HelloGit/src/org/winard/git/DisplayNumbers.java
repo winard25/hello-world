0235164198e9e3e20ac3667c491cc83df82f8633
winard25 <30685117+winard25@users.noreply.github.com>
2017-08-04 15:02:22 +0800

    update num to 20

--- HelloGit/src/org/winard/git/DisplayNumbers.java
+++ HelloGit/src/org/winard/git/DisplayNumbers.java
@@ -3,7 +3,7 @@ package org.winard.git;
 public class DisplayNumbers {
 	
 	public void generateNumbers() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			System.out.println(i);
 		}
 	}
